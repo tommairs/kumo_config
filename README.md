a8cfaad4cda41211e5e9d5a73433d4376baa23d1
Tom Mairs <tom@kumomta.com>
2023-11-17 20:51:18 +0000

    November update with current build edits.

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # Current CS dev/test config
-This is internal testing config only.  
+This is internal testing config only.  Shared for collaboration and debugging
 
-Shared for collaboration and debugging
+The use of this config and anything in this repo is strictly voluntary and 
+done at your own risk. 
 
-Use at your own risk.
+If you choose to use this config you MUST modify it with your own values before attempting to use it. 
 
-## Usage
+This is NOT a KumoMTA officialy supported config and is for experimental use only.  Feel free to copy 
+and alter it to suit your needs.
 
-First deploy KumoMTA as per [this](https://docs.kumomta.com/userguide/installation/linux/)
-
-Next clone this repo and copy it to `/opt/kumomta/etc/policy/`
-
-Change any settings you want to personalize it and restart (stop/start) KumoMTA
-
+~ Tom
+ 
 
--- init.lua
+++ init.lua
@@ -22,8 +22,9 @@ local dkim_sign = require 'policy-extras.dkim_sign'
 
 
 -- Create first basic webhook
-local wh_target = 'http://52.156.138.239:80/hud/collect/'
-k_helpers.create_webhook("webhook",wh_target,"my_user","my_pass",log_hooks)
+local wh_target = 'http://demo.kumomta.com:80//collector/'
+k_helpers.create_webhook("webhook",wh_target,"a_ebhook_user","a_webhook_password",log_hooks)
+
 
 -- Create second alternate webhook
 --[[ commented as this is only an example
@@ -95,7 +96,7 @@ kumo.on('init', function()
   kumo.start_http_listener {
     listen = '0.0.0.0:8000',
     -- allowed to access any http endpoint without additional auth
-    trusted_hosts = { '127.0.0.1', '::1', '52.156.138.239' },
+    trusted_hosts = { '127.0.0.1', '::1' },
   }
 
                 
@@ -103,9 +104,9 @@ kumo.on('init', function()
 local params = {
       listen = '0:25',
       relay_hosts = {'127.0.0.1'},
-      banner = "Big Island Surf Shop",
-      tls_private_key = "/opt/kumomta/etc/tls/my.demo.kumomta.com/ca.key",
-      tls_certificate = "/opt/kumomta/etc/tls/my.demo.kumomta.com/ca.crt",
+    --  banner = "My email server",
+    --  tls_private_key = "/opt/kumomta/etc/tls/my.demo.kumomta.com/ca.key",
+    --  tls_certificate = "/opt/kumomta/etc/tls/my.demo.kumomta.com/ca.crt",
     }
    kumo.start_esmtp_listener(params)
     
@@ -125,7 +126,7 @@ end) -- END OF THE INIT EVENT
 
 -- Load  Queue helper 
 -- You can pass multiple file names if desired
-local queue_helper = queue_module:setup({'/opt/kumomta/etc/policy/queues.toml'})
+ local queue_helper = queue_module:setup({'/opt/kumomta/etc/policy/queues.toml'})
 
 -- Load Listener Domains Helper
 kumo.on('get_listener_domain', listener_domains:setup { '/opt/kumomta/etc/policy/listener_domains.toml' } )
@@ -136,7 +137,7 @@ sources:setup { '/opt/kumomta/etc/policy/egress_sources.toml' }
 
 -- Load  Traffic Shaping Helper 
 local get_shaping_config = shaping:setup()
---kumo.on('get_egress_path_config', shaper.get_egress_path_config)
+kumo.on('get_egress_path_config', shaper.get_egress_path_config)
 
 
 -- Load DKIM Signing helper function
@@ -146,22 +147,38 @@ local dkim_signer = dkim_sign:setup({'/opt/kumomta/etc/policy/dkim_data.toml'})
 ----------------------------------------------------------------------------
 --[[ Determine queue routing ]]--
 ----------------------------------------------------------------------------
+--[[ Note that this section is ignored if you use the queue helper
 kumo.on('get_queue_config', function(domain, tenant, campaign)
 -- This is here in case you want to add any custom queue config
 
+print ("domain,tenant,campaign")
+print (domain,tenant,campaign)
+
+
+  if domain == 'gmail.com' then
+    -- Store this domain into a maildir, rather than attempting
+    -- to deliver via SMTP
+    return kumo.make_queue_config {
+      protocol = {
+        maildir_path = '/var/tmp/kumo-maildir',
+      },
+    }
+  end
+  -- Otherwise, just use the defaults
+
   return kumo.make_queue_config{}
 end)
+]]--
+
 
 ----------------------------------------------------------------------------
 --[[ Determine what to do on SMTP message reception ]]--
 ----------------------------------------------------------------------------
 kumo.on('smtp_server_message_received', function(msg)
 
-  queue_helper:apply(msg)
-  msg:remove_x_headers { 'x-tenant' }
-
 -----------------------------------------
-  -- added this to fix messageID issues
+  -- Added this to fix messageID issues
+  -- This shoudl always be loaded first
   local failed = msg:check_fix_conformance(
     -- check for and reject messages with these issues:
     'MISSING_COLON_VALUE',
@@ -173,39 +190,65 @@ kumo.on('smtp_server_message_received', function(msg)
   end
 -----------------------------------------
 
+  queue_helper:apply(msg)
+
+--[[ Sample code to add list-unsubscribe headers in-transit
+    local keyuserid = k_helpers.to_base64(msg:recipient().email .. msg:get_meta('tenant'))
+    msg:append_header("List-Unsubscribe", "<mailto:unsub@demo2.kumomta.com?subject=unsub>, <https://luna.kumomta.com/unsubscribe.php?t=" .. keyuserid .. ">")
+    msg:append_header("List-Unsubscribe-Post", "List-Unsubscribe=One-Click")
+]]--
+
+
+print ("DKIM signing message")
 -- SIGNING MUST COME LAST OR YOU COULD BREAK YOUR DKIM SIGNATURES
   dkim_signer(msg)
 end)
+
+
 
 ----------------------------------------------------------------------------
 --[[ Determine what to do on HTTP message reception ]]--
 ----------------------------------------------------------------------------
 kumo.on('http_message_generated', function(msg)
+
   queue_helper:apply(msg)
-  msg:remove_x_headers { 'x-tenant' }
 
 -- SIGNING MUST COME LAST OR YOU COULD BREAK YOUR DKIM SIGNATURES
   dkim_signer(msg)
 end)
+
 
 
 ----------------------------------------------------------------------------
 --[[ Adding basic Authentication (on disk) ]]--
 ----------------------------------------------------------------------------
 
+function sqlite_auth_check(user, password)
+  local db = sqlite.open '/home/myaccount/mypswd.db'
+  local result = db:execute ('select * from users where email=? and password=?', user ,password)
+
+  -- if we return the username, it is because the password matched
+  return result[1] == user
+end
+
 -- Cache the credentials so we are not constantly hitting the DB
-cached_sqlite_auth_check = kumo.memoize(k_helpers.sqlite_auth_check, {
+cached_sqlite_auth_check = kumo.memoize(sqlite_auth_check, {
   name = 'sqlite_auth',
   ttl = '5 minutes',
   capacity = 100,
 })
 
+
+
+--------------------------------------------------------------------------------
 -- Use this to lookup and confirm a user/password credential with the HTTP event
 kumo.on('http_server_validate_auth_basic', function(user, password)
 
   return cached_sqlite_auth_check(user, password)
 end)
 
+
+--------------------------------------------------------------------------------
 -- Use this to lookup and confirm a user/password credential with the SMTP event
 kumo.on('smtp_server_auth_plain', function(authz, authc, password)
 
